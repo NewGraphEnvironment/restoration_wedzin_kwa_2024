19253c9eff8c224eac6361867b6a29d5ff6ebcd1
almac2022 <al@newgraphenvironment.com>
2025-07-10 12:52:30 -0700

    crop raster to filled extent before slicing

--- scripts/gis/lidar_dl_split.R
+++ scripts/gis/lidar_dl_split.R
@@ -22,7 +22,8 @@ dir.create(path)
 
 dl <- "https://nrs.objectstore.gov.bc.ca/gdwuts/093/093l/2019/dem/bc_093l048_xli1m_utm09_2019.tif"
 dl <- "https://nrs.objectstore.gov.bc.ca/gdwuts/093/093l/2019/dem/bc_093l040_xli1m_utm09_2019.tif"
-
+dl <- "https://nrs.objectstore.gov.bc.ca/gdwuts/093/093l/2019/dem/bc_093l059_xli1m_utm09_2019.tif"
+# dl <- "https://nrs.objectstore.gov.bc.ca/gdwuts/093/093l/2018/dem/bc_093l059_xli1m_utm09_2018.tif"
 
 system.time(
   download.file(
@@ -33,50 +34,67 @@ system.time(
 
 
 # get the utm from the filename
-zone <- str_extract(basename(dl), "(?<=utm)\\d{2}") |>
+zone <- stringr::str_extract(basename(dl), "(?<=utm)\\d{2}") |>
   as.numeric()
 
 # get the bounding box of the tif file without reading it in
 path_lidar <- fs::path(path, basename(dl))
 tif <- terra::rast(path_lidar)
 
+# Set zero values to NA
+tif[tif == 0] <- NA
+
+# Crop to non-NA extent
+tif_cropped <- terra::trim(tif)
+
+# Get extent of non-zero values
+tif_bbox <- terra::ext(tif_cropped)
+
 # Extract the bounding box (extent)
-tif_bbox <- ext(tif)
+# tif_bbox <- terra::ext(tif)
 
 # Convert the bounding box to a SpatialPolygon and assign the original CRS
-tif_bbox_sp <- as.polygons(tif_bbox, crs = crs(tif))
+tif_bbox_sp <- terra::as.polygons(tif_bbox, crs = terra::crs(tif))
 
 # Transform the bounding box to EPSG 3005
-tif_bbox_sp_transformed <- project(tif_bbox_sp, "EPSG:3005")
+tif_bbox_sp_transformed <- terra::project(tif_bbox_sp, "EPSG:3005")
 
 # Convert to sf object (optional, for easier handling)
-tif_bbox_sf <- st_as_sf(tif_bbox_sp_transformed)
+tif_bbox_sf <- sf::st_as_sf(tif_bbox_sp_transformed)
 
-# get the grids that overlap and transform to the same zone (could use INTERSECTS except that
+# get the grids that overlap and transform to the same zone (could use INTERSECTS (vs WITHIN) except that
 # they don't line up even when we put our grid in the same crs as the tif.... wierd -not sure why)
 grid <- bcdata::bcdc_query_geodata("WHSE_BASEMAPPING.BCGS_5K_GRID") |>
-  filter(WITHIN(tif_bbox_sf)) %>%
-  collect() |>
+  bcdata::filter(bcdata::INTERSECTS(tif_bbox_sf)) |>
+  bcdata::collect() |>
   # sf::st_transform(crs = 32600 + zone)
   # sf::st_transform(2950 + (zone - 7))
 # NAD83(CSRS) / UTM zone 9N (EPSG:3156)
  sf::st_transform(3147 + zone)
 
+# burn to file to test
+# grid |>
+#   sf::st_write(
+#     fs::path(
+#       path_gis, "grid", ext="geojson"
+#     )
+#   )
+
 # visualize
 ggplot2::ggplot() +
-  geom_sf(data = grid, fill = "transparent", color = "red") +
-  geom_sf(data = tif_bbox_sf, fill = "transparent", color = "blue") +
-  theme_minimal()
+  ggplot2::geom_sf(data = grid, fill = "transparent", color = "red") +
+  ggplot2::geom_sf(data = tif_bbox_sf, fill = "transparent", color = "blue") +
+  ggplot2::theme_minimal()
 
 
 # split the tif into the grids
 split_raster <- function(tif, grid, path) {
   for (i in 1:nrow(grid)) {
     grid_i <- grid[i, ]
-    grid_i_sf <- st_as_sf(grid_i)
+    grid_i_sf <- sf::st_as_sf(grid_i)
     tif_i <- terra::crop(tif, grid_i_sf)
     # append the 1:5000 maptile name to the end of the details regarding resolution, utm and year
-    writeRaster(tif_i, filename = fs::path(path, paste0(str_extract(basename(dl), "xl[^.]*"), "_", grid_i$MAP_TILE, ".tif")), overwrite = TRUE)
+    terra::writeRaster(tif_i, filename = fs::path(path, paste0(stringr::str_extract(basename(dl), "xl[^.]*"), "_", grid_i$MAP_TILE, ".tif")), overwrite = TRUE)
   }
 }
 
@@ -86,6 +104,8 @@ split_raster(tif, grid, path)
 path_gis <- "~/Projects/gis/restoration_wedzin_kwa/lidar"
 file_cp <- "093L05822.tif"
 file_cp <- "093L04832.tif"
+file_cp_prep <- c("093L04031.tif", "093L04032.tif", "093L04033.tif", "093L04034.tif")
+file_cp <- paste0(stringr::str_extract(basename(dl), "xl[^.]*"), "_", file_cp_prep)
 fs::dir_create(path_gis)
 
 fs::file_copy(
